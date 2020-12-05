a681390442bbf6652803df4230f9f72047bc4909
Dragos Badea <dragostefan13@gmail.com>
2020-12-05 15:04:14 +0200

    day 5

--- day5_1.cpp
+++ day5_1.cpp
@@ -14,18 +14,16 @@ int main()
     int maxid = 0;
     while (f.getline(input, length + 1))
     {
-        //cout << input << '\n';
         int row = 0, seat = 0, id = 0;
         for (int k = 0; k < length - 3; ++k)
             if (input[k] == 'B')
                 row += pow(2, length - 4 - k);
-        //cout << row << '\n';
+        
         for (int k = length - 3; k < length; ++k)
             if (input[k] == 'R')
                 seat += pow(2, length - 1 - k);
-        //cout << seat << '\n';
+        
         id = row * 8 + seat;
-        //cout << id << '\n';
         if (id > maxid)
             maxid = id;
     }
